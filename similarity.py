bbe3cc3dc111fda8ed91dca3fcd19f9fa292706a
AlexandreCombeau <alexcombeau@outlook.fr>
2023-06-23 19:37:43 +0200

    Added Mutation and crossover methods

--- similarity.py
+++ similarity.py
@@ -23,6 +23,8 @@ def cos_similarity(x : str, y : str):
     tfidf_vectorizer = TfidfVectorizer()
     tfidf_matrix = tfidf_vectorizer.fit_transform([x, y])
     cosine_sim = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
+    if cosine_sim > 1: #fix because it can go to 1.0000002
+        cosine_sim == 1
     return cosine_sim
 
 
@@ -44,14 +46,14 @@ def jaro_similarity(x : str, y : str) -> float:
 #####################################
 ############### TODO ################
 #####################################
-
-
+#faire attention avec les distances
+#comprend prefix
 def jarowinkler_similarity(x : str, y : str) -> float:
     x = flatten(x)
     y = flatten(y)    
     jarowinkler = jellyfish.jaro_winkler_similarity(x,y)
     return jarowinkler
-
+#word2vec
 def euclidean_dist_similarity(x : str, y : str) -> float:
     pass
 
